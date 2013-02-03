53daf62d82652064f6bb38807371e87d78a26ce1
Wan Leung Wong <wanleung@linkomnia.com>
2013-02-03 17:56:42 +0800

    mv back to private rather than global, as the close will free the wrong memory. no idea why it would segfault. mine sys is ok no segfault

--- src/cangjie.h
+++ src/cangjie.h
@@ -43,6 +43,7 @@ class CangJie {
     private:
         DbEnv *cangjie_env_;
         Db* cangjie_db_;
+        Db* wordfreq_;
         CangJie_Version_Type cangjie_version_;
         uint32_t cangjie_flags_;
         std::vector<std::string> getCharactersRange (std::string begin, std::string ending);
@@ -50,6 +51,3 @@ class CangJie {
         CangJie() : cangjie_env_(0) {}
         void close();
 };
-
-        Db* wordfreq_;//FIXME: Try to add it to private header of class CangJie without segfault
-                      //       Testing on other system welcome.
