631fd1592de57ee82835119ce6041781f8993d5e
Wan Leung Wong <wanleung@linkomnia.com>
2013-03-12 14:07:21 +0800

    Bug fix obn test script after adding feature

--- tests/testdbs.pl
+++ tests/testdbs.pl
@@ -30,11 +30,11 @@ binmode(STDERR, ":utf8");
 
 my @tables = ("cj3-sc", "cj3-tc", "cj3-cc", "cj3-cjk",
               "cj5-sc", "cj5-tc", "cj5-cc", "cj5-cjk");
-
+my @additionals = ("jp");
 
 sub main {
     my $table = shift;
-
+    
     my $tablefilename = "tables/$table.txt";
     my $dbfilename = "data/$table.mb";
 
@@ -49,9 +49,70 @@ sub main {
     $tablefilename  = dirname(abs_path($0))."/../".$tablefilename;
 
     # Check that all the data from the text table file is in the DB
+    #my $in_data = 0;
+    #my $table_count = 0;
+    #open(FILE, "<:encoding(UTF-8)", "$tablefilename") or die($!);
+    #while (<FILE>) {
+    #    my $line = $_;
+
+    #    # Strip whitespace
+    #    $line=~s/^ +//;
+    #    $line=~s/ +$//;
+    #    $line=~s/\n+$//;
+
+    #    # Ignore empty and commented lines
+    #    if (($line =~ /^#/) || ($line eq "")) {
+    #        next;
+    #    }
+
+    #    if ($in_data) {
+    #        $table_count = $table_count + 1;
+
+    #        (my $key, my $table_value) = split(/ +/, $line);
+
+    #        my $db_value = encode_utf8($table_value);
+    #        $db->db_get($key, $db_value, DB_GET_BOTH);
+    #        if ($table_value ne decode_utf8($db_value)) {
+    #            die("Could not find ($key, $table_value)\n");
+    #        }
+    #    } else {
+    #        if ($line =~ /^\[DATA\]$/) {
+    #            # Next line is the first data line
+    #            $in_data = 1;
+    #        }
+    #    }
+    #}
+    my $table_count = readtable($db, $tablefilename);
+    my $add_table_count = 0; 
+    for my $addition (@additionals) {
+        my $additional_filename = "tables/$addition.txt";
+        $additional_filename  = dirname(abs_path($0))."/../".$additional_filename;
+        print $additional_filename;
+        my $m_count = readtable($db, $additional_filename);
+        $add_table_count += $m_count;
+    }
+
+
+    # Check that the DB has the same number of entries as the text table file
+    my $cursor = $db->db_cursor();
+
+    my $db_count = 0;
+    my $key = 0;
+    my $value = 0;
+    while ($cursor->c_get($key, $value, DB_NEXT) == 0) {
+        $db_count = $db_count+1;
+    }
+
+    if (($table_count + $add_table_count) != $db_count) {
+        die("Table contained $table_count entries but DB contains $db_count\n");
+    }
+}
+
+sub readtable {
+    my ($db, $filename) = @_;
     my $in_data = 0;
     my $table_count = 0;
-    open(FILE, "<:encoding(UTF-8)", "$tablefilename") or die($!);
+    open(FILE, "<:encoding(UTF-8)", "$filename") or die($!);
     while (<FILE>) {
         my $line = $_;
 
@@ -82,22 +143,12 @@ sub main {
             }
         }
     }
+    close FILE;
+    return $table_count;
 
-    # Check that the DB has the same number of entries as the text table file
-    my $cursor = $db->db_cursor();
-
-    my $db_count = 0;
-    my $key = 0;
-    my $value = 0;
-    while ($cursor->c_get($key, $value, DB_NEXT) == 0) {
-        $db_count = $db_count+1;
-    }
-
-    if ($table_count != $db_count) {
-        die("Table contained $table_count entries but DB contains $db_count\n");
-    }
 }
 
+#### Main Run Below
 
 for my $key (@tables) {
     main($key);
